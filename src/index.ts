16b72a4a4245587d5bb7f0b983fae89bb97f72b8
Wilfred Njenga <w.maina.njenga@gmail.com>
2023-10-01 23:26:50 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-// cannister code goes here
+class Candidate {
+  constructor(public name: string) {
+    this.votes = 0;
+  }
+  votes: number;
+}
+
+class ElectionSystem {
+  private candidates: Candidate[] = [];
+
+  addCandidate(candidateName: string) {
+    this.candidates.push(new Candidate(candidateName));
+  }
+
+  castVote(candidateName: string) {
+    const candidate = this.candidates.find((c) => c.name === candidateName);
+    if (candidate) {
+      candidate.votes++;
+      return true; // Vote cast successfully
+    }
+    return false; // Candidate not found
+  }
+
+  countVotes() {
+    const sortedCandidates = this.candidates.sort((a, b) => b.votes - a.votes);
+    return sortedCandidates;
+  }
+}
+
+// Example usage:
+const election = new ElectionSystem();
+election.addCandidate("Candidate A");
+election.addCandidate("Candidate B");
+
+election.castVote("Candidate A");
+election.castVote("Candidate A");
+election.castVote("Candidate B");
+
+const results = election.countVotes();
+console.log("Election Results:");
+results.forEach((candidate, index) => {
+  console.log(`${index + 1}. ${candidate.name}: ${candidate.votes} votes`);
+});
