bdb2a13ef8685ebf179104aeb0b1c10d2148c617
Adrian Danek <aadankan@gmail.com>
2024-03-28 18:04:10 +0100

    defined message type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-// cannister code goes here
+import { v4 as uuidv4 } from 'uuid';
+import { Server, StableBTreeMap, ic } from 'azle';
+import express from 'express';
+
+class Message {
+    id: string;
+    title: string;
+    body: string;
+    attachmentURL: string;
+    createdAt: Date;
+    updatedAt: Date | null
+ }
